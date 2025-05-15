c375e73697ea5acab649b21db61f02df72d0df6d
Galang Swastika Ramadhan <swastikaramadhan05@gmail.com>
2025-05-15 23:07:07 +0700

    update navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {auth, signOut, signIn} from '@/auth'
-import { sign } from 'crypto'
 
 const Navbar = async () => {
   const session = await auth(); 
